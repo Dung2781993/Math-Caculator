90108ea1ebeb93a34e8c58b590ef2364d4041b70
Dung2781993@gmail.com <Dung2781993@gmail.com>
2021-05-01 17:48:59 +1000

    Initial project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AllMediaDesk
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter math formula");
+            var input = Console.ReadLine();
+        }
+    }
+}

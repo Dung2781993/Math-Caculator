aff2902f6a4880837bf171d4f5b8700f5eed6651
Dung2781993@gmail.com <Dung2781993@gmail.com>
2021-05-01 17:59:15 +1000

    Update regex class

---
+++ RegexCalculator/RegexCalculator.cs
@@ -0,0 +1,15 @@
+﻿using AllMediaDesk.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AllMediaDesk.RegexCalculator
+{
+    class RegexCalculator : CalculatorBase
+    {
+        public override Number Calculate(string expression)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

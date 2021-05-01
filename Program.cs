bf4d1ad905bcaa00d65b6aa8566d791efe522a01
Dung2781993@gmail.com <Dung2781993@gmail.com>
2021-05-01 18:09:41 +1000

    Update math formula

--- Program.cs
+++ Program.cs
@@ -9,6 +9,10 @@ namespace AllMediaDesk
         {
             Console.WriteLine("Enter math formula");
             var input = Console.ReadLine();
+            var calculator = new RegexCalculator.RegexCalculator();
+            var result = calculator.Calculate(input);
+            Console.WriteLine("The answer is: " + result);
+            Console.WriteLine();
         }
     }
 }
